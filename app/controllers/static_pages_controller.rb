6dd96aba5766cd5e1801765d77e2a1d692bceac3
Gary Hess <garyhess999@hotmail.com>
2017-01-11 20:26:59 +0200

    cleaning up search page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,21 +4,18 @@ class StaticPagesController < ApplicationController
     # VERSION 1: plain search
 =begin
     @search = Sentence.search {fulltext params[:search]}
-    @results = @search.results
     @total_records = Sentence.count
 =end
 
     # VERSION 2: with highlighting
     # noticed that highlighting truncates search results, e.g. search for 'cnbc'
     @total_records = Sentence.count
-    @current_page = 1 # no pagination yet
     
     @search = Sentence.search do
       fulltext params[:search] do
         highlight :body
       end
     end
-    @results = @search.results
 
 
     # VERSION 3: with pagination  
